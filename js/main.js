f68839e4e80333bed3ed1e95a07cb2a0886cb957
Potetrompe <46726271+Potetrompe@users.noreply.github.com>
2019-01-21 17:46:27 +0100

    updated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 
+var slots = document.querySelectorAll("div");
+var revArr = [];
+
+for (var i = 0; i < slots.length; i++) {
+    revArr[i] = slots[i]; 
+}
+
+revArr.reverse();
+
+for (var i = 0; i < revArr.length; i++) {
+    revArr[i].style.order = i;
+}
